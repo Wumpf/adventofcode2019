25cae4ecafa800a8200593a35210d698a5c93a3c
Andreas Reich <r_andreas2@web.de>
2019-12-01 11:38:30 +0100

    day01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,24 +1,46 @@
-fn compute_fuel_from_mass(mass: i32) -> i32 {
+fn compute_fuel_from_mass_part1(mass: i32) -> i32 {
     mass / 3 - 2
+}
+
+fn compute_fuel_from_mass_part2(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    if fuel > 0 {
+        fuel + compute_fuel_from_mass_part2(fuel)
+    } else {
+        0
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn samples_part1() {
-        assert_eq!(super::compute_fuel_from_mass(12), 2);
-        assert_eq!(super::compute_fuel_from_mass(14), 2);
-        assert_eq!(super::compute_fuel_from_mass(1969), 654);
-        assert_eq!(super::compute_fuel_from_mass(100756), 33583);
+        assert_eq!(super::compute_fuel_from_mass_part1(12), 2);
+        assert_eq!(super::compute_fuel_from_mass_part1(14), 2);
+        assert_eq!(super::compute_fuel_from_mass_part1(1969), 654);
+        assert_eq!(super::compute_fuel_from_mass_part1(100756), 33583);
+    }
+
+    #[test]
+    fn samples_part2() {
+        assert_eq!(super::compute_fuel_from_mass_part2(14), 2);
+        assert_eq!(super::compute_fuel_from_mass_part2(1969), 966);
+        assert_eq!(super::compute_fuel_from_mass_part2(100756), 50346);
     }
 }
 
 fn main() {
     let puzzle_input = include_str!("input.txt");
-
-    let total_fuel: i32 = puzzle_input
+    let modules_mass = puzzle_input
         .lines()
-        .map(|line| compute_fuel_from_mass(line.parse::<i32>().unwrap()))
-        .sum();
-    println!("total fuel use is {}", total_fuel);
+        .map(|line| line.parse::<i32>().unwrap());
+
+    let mut total_fuel_part1 = 0;
+    let mut total_fuel_part2 = 0;
+    for mass in modules_mass {
+        total_fuel_part1 += compute_fuel_from_mass_part1(mass);
+        total_fuel_part2 += compute_fuel_from_mass_part2(mass);
+    }
+    println!("part1 {}", total_fuel_part1);
+    println!("part2 {}", total_fuel_part2);
 }
