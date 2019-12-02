db39cc7d1b5be1c36790584f2f6c27807dc26fb0
Andreas Reich <r_andreas2@web.de>
2019-12-02 20:34:53 +0100

    day02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -6,21 +6,21 @@ fn parse_program(puzzle_input: &str) -> Vec<usize> {
 }
 
 fn eval_program(program: &mut Vec<usize>) {
-    let mut ppos = 0;
-    while program[ppos] != 99 {
-        let op0 = program[program[ppos + 1]];
-        let op1 = program[program[ppos + 2]];
-        let target = program[ppos + 3];
-        program[target] = match program[ppos] {
-            1 => op0 + op1,
-            2 => op0 * op1,
+    let mut instrp = 0;
+    while program[instrp] != 99 {
+        let param0 = program[program[instrp + 1]];
+        let param1 = program[program[instrp + 2]];
+        let target_address = program[instrp + 3];
+        program[target_address] = match program[instrp] {
+            1 => param0 + param1,
+            2 => param0 * param1,
             _ => panic!("Invalid opcode!")
         };
-        ppos += 4;
+        instrp += 4;
     }
 }
 
-#[warn(dead_code)]
+#[allow(dead_code)]
 fn parse_and_eval_program(puzzle_input: &str) -> Vec<usize> {
     let mut program = parse_program(puzzle_input);
     eval_program(&mut program);
@@ -41,10 +41,26 @@ mod tests {
 
 fn main() {
     let puzzle_input = include_str!("input.txt");
+    let original_program = parse_program(puzzle_input);
 
-    let mut program = parse_program(puzzle_input);
-    program[1] = 12;
-    program[2] = 2;
-    eval_program(&mut program);
-    println!("part1 {}", program[0])
+    {
+        let mut program = original_program.clone();
+        program[1] = 12;
+        program[2] = 2;
+        eval_program(&mut program);
+        println!("part1 {}", program[0])
+    }
+
+    'noun: for noun in 0..100 {
+        'verb: for verb in 0..100 {
+            let mut program = original_program.clone();
+            program[1] = noun;
+            program[2] = verb;
+            eval_program(&mut program);
+            if program[0] == 19690720 {
+                println!("part2 {}", 100 * noun + verb);
+                break 'noun;
+            }
+        }
+    }
 }
